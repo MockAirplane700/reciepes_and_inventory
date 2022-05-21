b6048cc35d225e532e1e4df7c0cc5ae6928160dd
Mockairplane700 <sizibamthandazo@yahoo.com>
2022-05-21 00:11:24 -0500

    Began work on Card widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,6 +24,30 @@ class _HomeState extends State<Home> {
         child: ListView.builder(
             itemBuilder: (context, index) {
               //NULL CHECK
+              if (_list.isEmpty){
+                //the list is empty
+                return Center(
+                  child: Padding(padding: const EdgeInsets.all(5.0), child: Column(
+                    children: const [
+                      CircularProgressIndicator(),
+                      Text('The listing is currently empty, please wait',style: TextStyle(fontSize: 25,color: textColor),)
+                    ],
+                  ),),
+                );
+              }else{
+                //the list is not empty
+                return Card(
+                  color: cardColor,
+                  child: Padding(padding: const EdgeInsets.all(10.0), child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.network(_list[index].networkImage),
+                      // TODO: Make the card give the option to display the full recipe through an alert box
+                    ],
+                  ),),
+                );
+              }
             },
           itemCount: _list.length,
         ),
